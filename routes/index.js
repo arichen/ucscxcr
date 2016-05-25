23ed654713195de12e92d96aaa3cb96a48b57a3d
arichen <arichenyc@gmail.com>
2016-05-24 17:37:26 -0700

    Integrate with express file structure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 module.exports = router;
